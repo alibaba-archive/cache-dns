27e15c48d12fae17b6bd55cca82066076159757a
fengmk2 <fengmk2@gmail.com>
2013-08-02 16:03:55 +0800

    add lookup() support, just like dns.lookup() but cacheable

--- test/cache-dns.test.js
+++ test/cache-dns.test.js
@@ -62,28 +62,37 @@ describe('cache-dns.test.js', function () {
       });
     });
 
-    it('should return error when resolve 127.0.0.1', function (done) {
+    it('should return 127.0.0.1 when resolve 127.0.0.1', function (done) {
+      done = pedding(2, done);
       dns.resolve4('127.0.0.1', function (err, addresses) {
-        should.exists(err);
-        should.not.exists(addresses);
+        should.not.exists(err);
+        addresses.should.eql(['127.0.0.1']);
+        done();
+      });
+      dns.lookup('127.0.0.1', function (err, addresses) {
+        should.not.exists(err);
+        addresses.should.eql('127.0.0.1');
         done();
       });
     });
   });
 
-  describe('_updateCaches()', function () {
+  describe('resolve4() _updateCaches()', function () {
     before(function (done) {
       dns.resolve4('www.taobao.com', done);
     });
 
     it('should emit domain event when update timer trigger', function (done) {
-      dns.once('www.taobao.com', function (err, addresses) {
+      dns.once('www.taobao.com', function (err, addresses, method) {
+        method.should.equal('resolve4');
         should.not.exists(err);
         should.exists(addresses);
         addresses.length.should.above(0);
         // mock dns.resolve4 error
         mm.error(require('dns'), 'resolve4');
-        dns.once('www.taobao.com', function (err) {
+        dns.once('www.taobao.com', function (err, addresses, method) {
+          method.should.equal('resolve4');
+          should.exists(err);
           err.name.should.equal('MockError');
           err.message.should.equal('mm mock error');
           should.exists(err);
@@ -104,4 +113,45 @@ describe('cache-dns.test.js', function () {
       });
     });
   });
+
+  describe('lookup() _updateCaches()', function () {
+    var dns = CacheDNS.create({
+      cacheTime: 100, // 500ms
+    });
+
+    before(function (done) {
+      dns.lookup('www.taobao.com', done);
+    });
+
+    it('should emit domain event when update timer trigger', function (done) {
+      dns.once('www.taobao.com', function (err, addresses, method) {
+        method.should.equal('lookup');
+        should.not.exists(err);
+        should.exists(addresses);
+        addresses.length.should.above(0);
+        mm.error(require('dns'), 'lookup');
+        dns.once('www.taobao.com', function (err, addresses, method) {
+          method.should.equal('lookup');
+          should.exists(err);
+          err.name.should.equal('MockError');
+          err.message.should.equal('mm mock error');
+          should.exists(err);
+          mm.restore();
+
+          mm.data(require('dns'), 'lookup', []);
+          dns.once('www.taobao.com', function (err, addresses) {
+            should.not.exists(err);
+            addresses.should.eql([]);
+
+            dns.resolve4('www.taobao.com', function (err, addresses) {
+              should.not.exists(err);
+              addresses.length.should.above(0);
+              done();
+            });
+          });
+        });
+      });
+    });
+  });
+
 });
---
+++ test/hosts.js
@@ -0,0 +1,35 @@
+/*!
+ * cache-dns - hosts.js
+ * Copyright(c) 2013 fengmk2 <fengmk2@gmail.com>
+ * MIT Licensed
+ */
+
+"use strict";
+
+/**
+ * Module dependencies.
+ */
+
+var CacheDNS = require('../');
+
+var dns = CacheDNS.create({
+  cacheTime: 100, // 500ms
+});
+
+dns.resolve4('local.wo.taobao.com', function (err, addresses) {
+  console.log('resolve4: local.wo.taobao.com', err, addresses);
+
+  dns.resolve4('www.taobao.com', function (err, addresses) {
+    console.log('resolve4: www.taobao.com', err, addresses);
+
+    dns.lookup('local.wo.taobao.com', function (err, address) {
+      console.log('lookup: local.wo.taobao.com', err, address);
+
+      dns.lookup('www.taobao.com', function (err, address) {
+        console.log('lookup: www.taobao.com', err, address);
+
+        dns.close();
+      });
+    });
+  });
+});
